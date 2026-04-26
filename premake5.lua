f53acc39264f9ad084be65164605fc618fd815bc
Hawke <Howdytv@hotmail.com>
2026-04-26 18:38:22 +0100

    Premake

---
+++ premake5.lua
@@ -0,0 +1,122 @@
+workspace "HazelFollowAlong"
+	
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	outputdir = "%{cfg.system}-%{cfg.architecture}-%{cfg.buildcfg}"
+
+	project "Hazel"
+
+		location "Hazel"
+
+		kind "SharedLib"
+
+		language "C++"
+
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir	  ("obj/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp"
+		}
+
+		includedirs
+		{
+			"%{prj.name}/vendor/spdlog/include"
+		}
+
+		postbuildcommands
+		{
+			("{COPYFILE} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+		filter "system:windows"
+			
+			cppdialect "C++20"
+
+			staticruntime "On"
+
+			systemversion "latest"
+
+			buildoptions "/utf-8"
+			
+			defines
+			{
+				"HZ_PLATFORM_WINDOWS",
+				"HZ_BUILD_DLL"
+			}
+
+			filter "configurations:Debug"
+				defines "HZ_DEBUG"
+				symbols "On"
+			
+			filter "configurations:Release"
+				defines "HZ_RELEASE"
+				optimize "On"
+			
+			filter "configurations:Dist"
+				defines "HZ_DIST"
+				optimize "On"
+
+	project "Sandbox"
+		
+		location "Sandbox"
+
+		kind "ConsoleApp"
+
+		language "C++"
+
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir	  ("obj/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp"
+		}
+
+		includedirs
+		{
+			"Hazel/vendor/spdlog/include",
+			"Hazel/src"
+		}
+
+		links
+		{
+			"Hazel"
+		}
+
+		filter "system:windows"
+			
+			cppdialect "C++20"
+
+			staticruntime "On"
+
+			systemversion "latest"
+
+			buildoptions "/utf-8"
+			
+			defines
+			{
+				"HZ_PLATFORM_WINDOWS"
+			}
+
+			filter "configurations:Debug"
+				defines "HZ_DEBUG"
+				symbols "On"
+			
+			filter "configurations:Release"
+				defines "HZ_RELEASE"
+				optimize "On"
+			
+			filter "configurations:Dist"
+				defines "HZ_DIST"
+				optimize "On"
